96fe33a6f115899795de6c867195fa13dff7ce1e
aneskurtovic <anes.kurtovic@edu.fit.ba>
2019-09-28 16:42:27 +0200

    Dodana web-stranica

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const countriesSelectEl = document.getElementById('countries-select');
+const contentEl = document.getElementById('content');
+const countriesApiUrl = 'https://restcountries.eu/rest/v2/all';
+let countriesList = [];
+
+function UcitajDrzave(){
+    $.ajax({
+        url: 'https://restcountries.eu/rest/v2/all',
+        success: function(rez){
+            for (var i=0; i<rez.length; i++){
+                countriesList = rez;
+                const option = document.createElement('option');
+                option.value = rez[i].alpha2Code;
+                option.innerText = rez[i].name;
+                countriesSelectEl.appendChild(option);
+            }
+        }
+    })
+}
+
+UcitajDrzave();
+
+
+
+
+countriesSelectEl.onchange = function(e){
+    const countryCode = e.target.value;
+    const selectedCountry = countriesList.find(c => c.alpha2Code === countryCode);
+    if (selectedCountry){
+        var formatiranjeBroja = new Intl.NumberFormat();
+        
+
+        const content = `
+        <img src="${selectedCountry.flag}" class="card front" />
+        <div class="card back padding">
+          <p><u>Glavni grad:</u> ${selectedCountry.capital}</p>
+          <p><u>Broj stanovnika:</u> ${formatiranjeBroja.format(selectedCountry.population)}</p>
+          <p><u>Domena:</u> ${selectedCountry.topLevelDomain}</p>
+          <p><u>Pozivni broj:</u> +${selectedCountry.callingCodes[0]}</p>
+        </div>
+      `;
+      contentEl.innerHTML = content;
+    }
+}
+
